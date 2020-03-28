4e4bb5d77c43e8e3c867b8a6f7ea88f29108291a
comp500 <comp500@users.noreply.github.com>
2020-03-28 03:02:09 +0000

    Add boilerplate stuff

---
+++ src/main/java/link/infra/funkyforcefields/funkyforcefields/FunkyForcefields.java
@@ -0,0 +1,10 @@
+package link.infra.funkyforcefields.funkyforcefields;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FunkyForcefields implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
