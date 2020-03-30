fd757e105b8cd220d72fda030a041216287cff27
comp500 <comp500@users.noreply.github.com>
2020-03-30 02:57:59 +0100

    Add cotton resources as runtime dep, remove outline of forcefields

---
+++ src/main/java/link/infra/funkyforcefields/mixin/WorldRendererMixin.java
@@ -0,0 +1,24 @@
+package link.infra.funkyforcefields.mixin;
+
+import link.infra.funkyforcefields.FunkyForcefields;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+	// Remove block outline for forcefields, as getOutlineShape affects raytracing
+	@Inject(at = @At("HEAD"), method = "drawBlockOutline(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/entity/Entity;DDDLnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V", cancellable = true)
+	private void onDrawBlockOutline(MatrixStack matrixStack, VertexConsumer vertexConsumer, Entity entity, double d, double e, double f, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
+		if (blockState.getBlock().equals(FunkyForcefields.VERTICAL_FORCEFIELD)) {
+			ci.cancel();
+		}
+	}
+}
